ab34cbbb2924c9701c2390b24cf83bd74c4bc228
hoshikoo <hoshikooki@gmail.com>
2015-03-13 01:31:45 -0400

    add exit ticket code

---
+++ src/nyc/c4q/hoshikoo/ExitTicket.java
@@ -0,0 +1,15 @@
+package nyc.c4q.hoshikoo;
+
+/**
+ * Created by Hoshiko on 3/12/15.
+ */
+public class ExitTicket {
+    public static void hello(String name){
+        System.out.println("Hi, "+ name);
+    }
+
+
+    public static void main(String[] args) {
+        hello("Alex");
+    }
+}
